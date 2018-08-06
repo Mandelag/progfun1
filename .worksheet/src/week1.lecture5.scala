7d132ff8291ea7a54e62289c959210d158e98729
Keenan Gebze <keenan.gebze@gmail.com>
2018-08-07 00:16:50 +0700

    Initial commit!

---
+++ .worksheet/src/week1.lecture5.scala
@@ -0,0 +1,16 @@
+package week1
+
+object lecture5 {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(76); 
+  println("Welcome to the Scala worksheet");$skip(47); 
+  
+  def abs(x: Double) = if (x < 0) -x else x;System.out.println("""abs: (x: Double)Double""");$skip(60); 
+  
+  def sqrtIter(guess: Double, value: Double) = {
+  	
+  };System.out.println("""sqrtIter: (guess: Double, value: Double)Unit""");$skip(37); 
+  
+  
+  
+  def sqrt(x: Double) = ???;System.out.println("""sqrt: (x: Double)Nothing""")}
+  
+}
---
+++ src/week1/lecture5.sc
@@ -0,0 +1,16 @@
+package week1
+
+object lecture5 {
+  println("Welcome to the Scala worksheet")       //> Welcome to the Scala worksheet
+  
+  def abs(x: Double) = if (x < 0) -x else x       //> abs: (x: Double)Double
+  
+  def sqrtIter(guess: Double, value: Double) = {
+  	
+  }                                               //> sqrtIter: (guess: Double, value: Double)Unit
+  
+  
+  
+  def sqrt(x: Double) = ???                       //> sqrt: (x: Double)Nothing
+  
+}
