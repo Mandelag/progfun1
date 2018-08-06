0325239374eb90a42d3c050a40a3c6cb624142aa
Keenan Gebze <keenan.gebze@gmail.com>
2018-08-07 00:24:17 +0700

    Implementing method for lecture 5

--- .worksheet/src/week1.lecture5.scala
+++ .worksheet/src/week1.lecture5.scala
@@ -1,16 +1,23 @@
 package week1
 
-object lecture5 {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(76); 
+object lecture5 {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(54); 
+  val EPSILON = 0.001;System.out.println("""EPSILON  : Double = """ + $show(EPSILON ));$skip(44); 
   println("Welcome to the Scala worksheet");$skip(47); 
   
-  def abs(x: Double) = if (x < 0) -x else x;System.out.println("""abs: (x: Double)Double""");$skip(60); 
+  def abs(x: Double) = if (x < 0) -x else x;System.out.println("""abs: (x: Double)Double""");$skip(148); 
   
-  def sqrtIter(guess: Double, value: Double) = {
-  	
-  };System.out.println("""sqrtIter: (guess: Double, value: Double)Unit""");$skip(37); 
+  def sqrtIter(guess: Double, value: Double):Double = {
+  	if (isGoodEnough(guess, value)) guess else sqrtIter(improve(guess, value), value)
+  };System.out.println("""sqrtIter: (guess: Double, value: Double)Double""");$skip(108); 
   
+  def isGoodEnough(guess: Double, value: Double):Boolean = {
+  	abs(guess * guess - value) < EPSILON
+  };System.out.println("""isGoodEnough: (guess: Double, value: Double)Boolean""");$skip(92); 
   
+  def improve(guess: Double, value: Double):Double = {
+    (guess + value/guess) / 2
+  };System.out.println("""improve: (guess: Double, value: Double)Double""");$skip(44); 
   
-  def sqrt(x: Double) = ???;System.out.println("""sqrt: (x: Double)Nothing""")}
+  def sqrt(x: Double) = sqrtIter(1.0, x);System.out.println("""sqrt: (x: Double)Double""")}
   
 }
--- src/week1/lecture5.sc
+++ src/week1/lecture5.sc
@@ -1,16 +1,23 @@
 package week1
 
 object lecture5 {
+  val EPSILON = 0.001                             //> EPSILON  : Double = 0.001
   println("Welcome to the Scala worksheet")       //> Welcome to the Scala worksheet
   
   def abs(x: Double) = if (x < 0) -x else x       //> abs: (x: Double)Double
   
-  def sqrtIter(guess: Double, value: Double) = {
-  	
-  }                                               //> sqrtIter: (guess: Double, value: Double)Unit
+  def sqrtIter(guess: Double, value: Double):Double = {
+  	if (isGoodEnough(guess, value)) guess else sqrtIter(improve(guess, value), value)
+  }                                               //> sqrtIter: (guess: Double, value: Double)Double
   
+  def isGoodEnough(guess: Double, value: Double):Boolean = {
+  	abs(guess * guess - value) < EPSILON
+  }                                               //> isGoodEnough: (guess: Double, value: Double)Boolean
   
+  def improve(guess: Double, value: Double):Double = {
+    (guess + value/guess) / 2
+  }                                               //> improve: (guess: Double, value: Double)Double
   
-  def sqrt(x: Double) = ???                       //> sqrt: (x: Double)Nothing
+  def sqrt(x: Double) = sqrtIter(1.0, x)          //> sqrt: (x: Double)Double
   
 }
